7e02912db9e79c4a9b5e38da1cd680a1e131b8d0
andreytsvl15 <andreytsvl15@gmail.com>
2023-12-18 14:15:52 +0300

    DZ6_1

---
+++ less_6/Program.cs
@@ -0,0 +1,26 @@
+﻿class Program
+{
+    static void Main()
+    {
+        // Создаем двумерный массив символов
+        char[,] array = new char[,]
+        {
+            { 'H', 'e', 'l', 'l', 'o' },
+            { 'W', 'o', 'r', 'l', 'd' }
+        };
+
+        // Создаем строку из символов массива
+        string str = "";
+
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                str += array[i, j];
+            }
+        }
+
+        // Выводим строку на экран
+        Console.WriteLine(str);
+    }
+}
