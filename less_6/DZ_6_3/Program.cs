bd1c7ae57fa44950166465d18f2e455f4f6734ce
andreytsvl15 <andreytsvl15@gmail.com>
2023-12-18 14:30:05 +0300

    DZ6_3

---
+++ less_6/DZ_6_3/Program.cs
@@ -0,0 +1,42 @@
+﻿
+class Program
+{
+    static void Main()
+    {
+        string inputString = "level";
+        
+        bool isPalindrome = IsPalindrome(inputString);
+
+        if (isPalindrome)
+        {
+            Console.WriteLine("Строка является палиндромом");
+        }
+        else
+        {
+            Console.WriteLine("Строка не является палиндромом");
+        }
+    }
+
+    static bool IsPalindrome(string input)
+    {
+        // Удаляем все пробелы из строки и приводим к нижнему регистру
+        input = input.Replace(" ", "").ToLower();
+
+        // Проверяем, является ли строка палиндромом
+        int left = 0;
+        int right = input.Length - 1;
+
+        while (left < right)
+        {
+            if (input[left] != input[right])
+            {
+                return false;
+            }
+
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+}
