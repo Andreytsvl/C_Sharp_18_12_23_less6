09ac36042d2ada311fe2899bca29fa32a79ffa94
andreytsvl15 <andreytsvl15@gmail.com>
2023-12-18 14:24:14 +0300

    DZ6_2

---
+++ less_6/DZ6_2/Program.cs
@@ -0,0 +1,30 @@
+﻿class Program
+{
+    static void Main()
+    {
+        string inputString = "HeLLO WoRLd";
+        string outputString = ConvertToUpperToLower(inputString);
+
+        Console.WriteLine(outputString);
+    }
+
+    static string ConvertToUpperToLower(string input)
+    {
+        string result = "";
+
+        foreach (char c in input)
+        {
+            if (Char.IsUpper(c))
+            {
+                char lower = Char.ToLower(c);
+                result += lower;
+            }
+            else
+            {
+                result += c;
+            }
+        }
+
+        return result;
+    }
+}
